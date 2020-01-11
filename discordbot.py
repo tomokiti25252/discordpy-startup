79cc38726bf645016a117408b5fd1182fb05a363
tomokiti25252 <59505487+tomokiti25252@users.noreply.github.com>
2020-01-11 22:23:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,6 +19,7 @@ client = discord.Client()
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
 edit_flag = True
+edit_flag2 = True
 
 test_ch = None
 test_ch_numch = None
@@ -337,27 +338,32 @@ async def on_message(message):
 @client.event
 async def on_message_edit(before,after):
     global edit_flag
+    global edit_flag2
     if edit_flag == True:
 
         if after.channel == t_ch and t_flag == True and after.embeds[0].description and before.embeds != after.embeds:
             edit_flag=False
             if "正解" in after.embeds[0].description:
                 await t_ch.send("::t Training")
+            await asyncio.sleep(0.2)
+            edit_flag = True
 
-            if after.embeds and after.embeds[0].description:
-                if f"{client.user.mention}はレベルアップした！" in after.embeds[0].description:
-                    dateTime = datetime.now(JST)
-                    lv = after.embeds[0].description.split("`")[1]
-                    embed = discord.Embed(
-                        title = "━LvUP━",
-                        description = f"**__{lv}__**",
-                        color = discord.Color.green())
-                    embed.set_footer(text = f"{dateTime.year}年{dateTime.month}月{dateTime.day}日　{dateTime.hour}時{dateTime.minute}分{dateTime.second}秒")
-                    \
-                    await asyncio.gather(*(c.send(embed=embed) for c in client.get_all_channels() if c.name == 'botレベルアップログ'))
+    if edit_flag2 == True:
+        if after.embeds and after.embeds[0].description:
+            if f"{client.user.mention}はレベルアップした！" in after.embeds[0].description:
+                edit_flag2=False
+                dateTime = datetime.now(JST)
+                lv = after.embeds[0].description.split("`")[1]
+                embed = discord.Embed(
+                    title = "━LvUP━",
+                    description = f"**__{lv}__**",
+                    color = discord.Color.green())
+                embed.set_footer(text = f"{dateTime.year}年{dateTime.month}月{dateTime.day}日　{dateTime.hour}時{dateTime.minute}分{dateTime.second}秒")
+                \
+                await asyncio.gather(*(c.send(embed=embed) for c in client.get_all_channels() if c.name == 'botレベルアップログ'))
+            await asyncio.sleep(0.2)
+            edit_flag2 = True
 
 
-        await asyncio.sleep(0.1)
-        edit_flag = True
 
 client.run(TOKEN)
